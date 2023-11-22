18abd1930151c30651d98e0336cd23d092e07e3f
Valdsonmacedo <Valdsonmacedo15@gmail.com>
2023-11-22 16:09:16 -0300

    Tela de acesso ao crud implementada

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,33 @@
 import "react";
 import "./Header.style.css"
 import logo from "../../assets/gigapizza_logo.svg"
+import { useState } from "react";
 
 function Header() {
+
+    const [state, setState] = useState(false)
+
+    const handleState = (event) => {
+        event.preventDefault()
+        setState(!state)
+    }
+
+    console.log(state)
+
     return (
         <header className="cabecalho">
             <img src={logo} alt="logo gigapizza" className="logo"/>
 
             <div className="botoes_container">
-                <button className="botao_cadastros">Novos cadastros</button>
+                <button className="botao_cadastros"
+                        onClick={handleState}>
+                            Novos cadastros
+                </button>
 
-                <button className="botao_cadastros">Itens cadastrados</button>
+                <button className="botao_itens_cadastros">Itens cadastrados</button>
 
-                <nav className="navegacao">
+                <nav className={`navegacao ${state ? 'on' : ''}`}>
+
                     <a href="#" className="sabor">Sabor</a>
                     <div className="barrinha_nav"/>
                     <a href="#" className="itens">Itens</a>
