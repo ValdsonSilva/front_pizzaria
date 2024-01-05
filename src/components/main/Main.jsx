f1d6a107515864977372e8ec02c1e437826754c4
Valdsonmacedo <Valdsonmacedo15@gmail.com>
2024-01-05 14:05:11 -0300

    Alterando tela do adm novamente

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -4,20 +4,27 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useContext } from "react";
 import { pedidosContext } from "../../context/pedidoscontext";
+import Logo from "../../assets/Giga-Photoroom 3.svg"
+import Footer from "../footer/Footer";
+
 function Main({child}) {
     const {carrinhopedidos, mypizzas} = useContext(pedidosContext)
     carrinhopedidos.BuscarPedidos()
     console.log(mypizzas)
     return (
-        <main className="conteudo">
-            {child}
-            {/* link para a tela de pedidos */}
-            <div className="container-contagem-pedidos">
-                <Link to='/pedidos' className="botao-pedidos">
-                    Pedidos
-                </Link>
-                <div className="contador-pedidos">{carrinhopedidos.tamanhopedidos}</div>
+        <main className="conteudoContainer">
+            <div className="conteudo">
+                {child}
+                {/* link para a tela de pedidos */}
+                <div className="container-contagem-pedidos">
+                    <Link to='/pedidos' className="botao-pedidos">
+                        Pedidos
+                    </Link>
+                    <div className="contador-pedidos">{carrinhopedidos.tamanhopedidos}</div>
+                </div>
             </div>
+            <img src={Logo} className=".GigaLogoGrande" alt="Imagem grande da logo do giga pizza" />
+            <Footer/>
         </main>
     )
 }
