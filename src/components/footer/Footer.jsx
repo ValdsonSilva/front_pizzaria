89be281b7746fd9f59afe6c65e72d147b918b1bc
Valdsonmacedo <Valdsonmacedo15@gmail.com>
2024-01-05 11:28:07 -0300

    feat(usuários): Adicionando imagem de fundo em todas as telas

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.style.css"
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p className="footerTextp">Developed by: TADS IV</p>
+        </div>
+    )
+}
+
+export default Footer
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,19 +2,24 @@ import "react";
 import "./Main.style.css"
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import Gigaphoto from "../../assets/Giga-PhotoRoom 3.svg";
+import Footer from "../footer/Footer";
 
 function Main({child}) {
 
     return (
-        <main className="conteudo">
-            {child}
-            {/* link para a tela de pedidos */}
-            <div className="container-contagem-pedidos">
-                <Link to='/pedidos' className="botao-pedidos">
-                    Pedidos
-                </Link>
-                <div className="contador-pedidos">0</div>
+        <main className="conteudoContainer">
+            <div className="conteudo">
+                {/* link para a tela de pedidos */}
+                <div className="container-contagem-pedidos">
+                    <Link to='/pedidos' className="botao-pedidos">
+                        Pedidos
+                    </Link>
+                    <div className="contador-pedidos">0</div>
+                </div>
             </div>
+            <img src={Gigaphoto} alt="Logo grande do gigapizza" className="GigaLogoGrande"/>
+            <Footer/>
         </main>
     )
 }
